653b454212c7ab395783b15e32e86a9ba4982453
Maxim Kastorsky <104034829+Olsney@users.noreply.github.com>
2025-07-11 03:25:44 +0300

    Refactored a few

--- src/RaccoonsTestTask/Assets/Code/Gameplay/Cubes/Spawner/CubeSpawner.cs
+++ src/RaccoonsTestTask/Assets/Code/Gameplay/Cubes/Spawner/CubeSpawner.cs
@@ -10,7 +10,7 @@ namespace Code.Gameplay.Cubes.Spawner
 {
     public class CubeSpawner : MonoBehaviour
     {
-        private const float ForceModifier = 0.7f;
+        private const float ForceModifier = 3f;
         private IPlayerInputHandlerProvider _playerInputHandlerProvider;
         private IGameFactory _gameFactory;
         private PlayerInputHandler _playerInputHandler;
@@ -19,22 +19,22 @@ namespace Code.Gameplay.Cubes.Spawner
         private static readonly Dictionary<int, Color> _colorByCubeValue = new()
         {
             { 2, new Color(0.85f, 0.92f, 0.98f) },
-            { 4, new Color(0.50f, 0.78f, 0.96f) }, 
-            { 8,     new Color(0.96f, 0.80f, 0.60f) }, 
-            { 16,    new Color(0.98f, 0.68f, 0.42f) }, 
-            { 32,    new Color(0.98f, 0.48f, 0.37f) }, 
-            { 64,    new Color(0.94f, 0.30f, 0.23f) }, 
-            { 128,   new Color(0.93f, 0.81f, 0.45f) }, 
-            { 256,   new Color(0.93f, 0.78f, 0.31f) },
-            { 512,   new Color(0.93f, 0.76f, 0.15f) },
-            { 1024,  new Color(0.93f, 0.69f, 0.07f) },
-            { 2048,  new Color(0.90f, 0.60f, 0.00f) },
-            { 4096,  new Color(0.45f, 0.36f, 0.74f) },
-            { 8192,  new Color(0.33f, 0.60f, 0.78f) },
+            { 4, new Color(0.50f, 0.78f, 0.96f) },
+            { 8, new Color(0.96f, 0.80f, 0.60f) },
+            { 16, new Color(0.98f, 0.68f, 0.42f) },
+            { 32, new Color(0.98f, 0.48f, 0.37f) },
+            { 64, new Color(0.94f, 0.30f, 0.23f) },
+            { 128, new Color(0.93f, 0.81f, 0.45f) },
+            { 256, new Color(0.93f, 0.78f, 0.31f) },
+            { 512, new Color(0.93f, 0.76f, 0.15f) },
+            { 1024, new Color(0.93f, 0.69f, 0.07f) },
+            { 2048, new Color(0.90f, 0.60f, 0.00f) },
+            { 4096, new Color(0.45f, 0.36f, 0.74f) },
+            { 8192, new Color(0.33f, 0.60f, 0.78f) },
             { 16384, new Color(0.24f, 0.67f, 0.47f) },
             { 32768, new Color(0.85f, 0.28f, 0.36f) },
             { 65536, new Color(0.53f, 0.24f, 0.75f) },
-            { 131072,new Color(0.28f, 0.56f, 0.76f) },
+            { 131072, new Color(0.28f, 0.56f, 0.76f) },
         };
 
 
@@ -91,13 +91,13 @@ namespace Code.Gameplay.Cubes.Spawner
         {
             float min = -1f;
             float max = 1f;
-            
+
             Vector3 randomDirection = Vector3.up + new Vector3(
                 _randomService.Next(min, max),
-                _randomService.Next(0, max),
+                _randomService.Next(min, max),
                 _randomService.Next(min, max)
             ).normalized;
-            
+
             rigidbody.AddForce(randomDirection * ForceModifier, ForceMode.Impulse);
         }
     }
